09b0f399669c14d352003808c51ef1a9c3cdadb7
honestlee213 <sehwan@thinkhei.com>
2024-10-24 17:21:04 +0900

    PICNIC 끝!!

--- problem-solving-strategy/PICNIC.cpp
+++ problem-solving-strategy/PICNIC.cpp
@@ -4,12 +4,28 @@ using namespace std;
 
 int res = 0;
 
-int mate(vector<int> kid, vector<vector<int>> friendship) {
+void mate(vector<int> kid, vector< vector<int> > friendship) {
     int n = kid.size();
     int target;
 
     for (int i = 0; i < n; i++) {
-        // if ()
+        if (kid[i] == 0) {
+            target = i;
+            break;
+        } else if (i == n - 1) {
+            res++;
+            return;
+        }
+    }
+
+    for (int i = target + 1; i < n; i++) {
+        if (kid[i] == 0 && friendship[target][i] == 1) {
+            kid[target] = 1;
+            kid[i] = 1;
+            mate(kid, friendship);
+            kid[target] = 0;
+            kid[i] = 0;
+        }
     }
 }
 
@@ -25,7 +41,7 @@ int main() {
         cin >> m;
 
         vector<int> kid(n, 0);
-        vector<vector<int>> friendship(n, vector<int>(n, 1));
+        vector< vector<int> > friendship(n, vector<int>(n, 0));
 
         for (int j = 0; j < m; j++) {
             int kid_1, kid_2;
@@ -37,8 +53,13 @@ int main() {
             friendship[kid_2][kid_1] = 1;
         }
 
-        // mate()
+        mate(kid, friendship);
+        result[i] = res;
+        res = 0;
     }
+
+    for (int i = 0; i < c; i++)
+        cout << result[i] << endl;
 
     return 0;
 }
