db9a71d248134942fa26ba6e8d56ef7766ac9032
honestlee213 <sehwan@thinkhei.com>
2024-10-24 14:52:10 +0900

    Update PICNIC.cpp

--- problem-solving-strategy/PICNIC.cpp
+++ problem-solving-strategy/PICNIC.cpp
@@ -2,8 +2,15 @@
 #include <vector>
 using namespace std;
 
-int friendship(int ) {
+int res = 0;
 
+int mate(vector<int> kid, vector<vector<int>> friendship) {
+    int n = kid.size();
+    int target;
+
+    for (int i = 0; i < n; i++) {
+        // if ()
+    }
 }
 
 int main() {
@@ -11,23 +18,26 @@ int main() {
 
     cin >> c;
 
+    vector<int> result(c, 0);
+
     for (int i = 0; i < c; i++) {
         cin >> n;
         cin >> m;
 
-        vector<vector<int>> kid(n, vector<int>(1, 0));
-        int kid_1, kid_2;
+        vector<int> kid(n, 0);
+        vector<vector<int>> friendship(n, vector<int>(n, 1));
+
         for (int j = 0; j < m; j++) {
+            int kid_1, kid_2;
+
             cin >> kid_1;
             cin >> kid_2;
 
-            kid[kid_1].
-            kid[kid_2][kid_1] = 1;
+            friendship[kid_1][kid_2] = 1;
+            friendship[kid_2][kid_1] = 1;
         }
 
-        for (int j = 0; j < n; j++) {
-            
-        }
+        // mate()
     }
 
     return 0;
